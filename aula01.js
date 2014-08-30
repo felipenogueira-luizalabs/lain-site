832d47376d10198ca7bfab77d5934a35c70e34ec
Felipe Bezerra <felipe_angells@hotmail.com>
2014-08-30 19:42:33 -0300

    First commit

---
+++ aula01.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var arquivo = require('fs');
+var url = require('url');
+
+var server = http.createServer(function(request, response){
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write("<h1>Hello World!</h1>");
+	response.write("<br>");	
+	response.write("<h3>Aulas NodeJS</h3>");	
+
+	var txt = arquivo.createWriteStream('./arquivos/logs.txt', {flags: 'a'});
+	txt.write(request.url+'\n');
+
+	var result = url.parse(request.url);
+
+	console.log(result.host);
+	console.log(result.port);
+	
+	response.end();		
+});
+
+server.listen(3000, function(){
+	console.log('Servidor está rodando!');	
+});
